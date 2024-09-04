721ddb342c7588a89fb793134f63e847ce9deeb2
Arthur Lopez <arthur.lopez@outlook.com>
2024-09-04 14:58:31 +0200

    Dashboard stremlit

--- code_alongs/03_stream_intro/dashboard.py
+++ code_alongs/03_stream_intro/dashboard.py
@@ -15,6 +15,21 @@ def read_data():
     df.index = df.index.year
     return df
 
+def layout():
+    df = read_data()
+    st.markdown("# YH dashboard")
+    st.markdown("This is a simple dashboard about yrkerhögskola")
+    
+    st.markdown("## Raw data")
+    st.markdown("Data shows started educations per region per year")
+    st.dataframe(df)
+
+# __name__ is a special variable, which is equal to '__main__' when we run this script
+# when we import this script from elsewhere, __name__ is the scripts name
+
+if __name__ == '__main__':
+    layout()
+
 print(read_data())
 
 
