2c4cea8983cfd107de18c4293bce505a497e543a
Arthur Lopez <arthur.lopez@outlook.com>
2024-10-06 11:28:29 +0200

    duckdb with python

--- yt_course/08_analyse_duckdb/Scripts/EDA.sql
+++ yt_course/08_analyse_duckdb/Scripts/EDA.sql
@@ -22,9 +22,59 @@ SELECT * FROM youtube;
 
 
 SELECT
-	*
+	* EXCLUDE ("youtuber name", "avg views")
 FROM
 	youtube
 WHERE
 	category = 'Science & Technology'
 	OR category = 'Education';
+	
+
+SELECT DISTINCT "audience country" FROM youtube;  
+
+
+SELECT * FROM youtube WHERE "audience country" = 'Japan';
+
+SELECT
+	"audience country",
+	COUNT(*) AS number_youtubers
+FROM
+	youtube
+GROUP BY
+	"audience country"
+ORDER BY
+	number_youtubers DESC
+LIMIT 10
+;
+
+
+SELECT
+	category,
+	COUNT(*) AS number
+FROM
+	youtube
+GROUP BY
+	category
+ORDER BY
+	number DESC;
+
+
+SELECT DISTINCT "avg comments" FROM youtube;
+
+
+SELECT
+	"youtuber name",
+	CASE
+		WHEN "avg comments" = 'N/A' THEN 0
+		WHEN "avg comments" LIKE '%K' THEN CAST(REPLACE("avg comments",
+		'K',
+		'') AS FLOAT) * 1000
+		ELSE CAST("avg comments" AS INTEGER) 
+	END AS avg_comments,
+	"avg comments"
+FROM
+	youtube
+ORDER BY avg_comments DESC;
+
+
+SELECT * FROM youtube;
---
+++ yt_course/09_python_connect_duckdb/database.py
@@ -0,0 +1,14 @@
+import duckdb
+
+#class Database:
+    
+ #   def __init__(self) -> None:
+      #  pass
+    
+  #  def __enter__(self):
+    #    pass
+    
+   # def __init__(self, exc_type, exc):
+     #   pass 
+
+    
